5b549394c57805a059d151cbdd0d55e0156a5372
jerslf <jerslf@hotmail.co.uk>
2025-11-04 16:05:42 +0100

    Exchanges and Queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,6 +20,23 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+
+	// publish a message to the exchange
+	err = pubsub.PublishJSON(
+		ch,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: true},
+	)
+	if err != nil {
+		log.Printf("failed to publish pause message: %v", err)
+	}
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
